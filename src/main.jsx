2dfe72a34b7c2c70df67a2c8bb52d254e87d851b
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-24 12:31:56 +0530

    refactor: place all routes in a separate file

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App";
-import Profile from "./components/Profile";
-import ErrorPage from "./components/ErrorPage";
+import routes from "./routes";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "profile/:name",
-    element: <Profile />,
-  },
-]);
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>
+  </StrictMode>,
 );
